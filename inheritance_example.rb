5d48670c0b5fd5a00ecb0e5621f677843962009f
Tom <tchulick48@gmail.com>
2020-02-26 16:17:58 -0600

    Tested various things

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -6,10 +6,18 @@
 # • To test it, create a bicycle and car instance from your newly modified classes (you can create them at the bottom of your file).
 # First, have them accelerate. Then, make sure a bike can “Ring ring!” and a car "Beeeeeeep!"
 
+# Exercise:
+# Open the inheritance_example.rb file from the previous example.
+# Add car specific attributes (instance variables) to the Car class (fuel, make, model, etc.)
+# and bicycle specific attributes to the Bicycle class (type, weight, etc.).
+# Use the super method to keep the initialize methods DRY.
+
 class Auto
-  def initialize
-    @speed = 0
-    @direction = "north"
+  def initialize(input)
+    @speed = input[:speed]
+    @direction = input[:direction]
+    # @speed = 0
+    # @direction = "north"
   end
 
   def brake
@@ -27,18 +35,30 @@ class Auto
 end
 
 class Car < Auto
+  # def initialize
+  #   super()
+  #   @fuel =
+  # end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Auto
+  # def initialize
+  #   super(input)
+  #   @type = "bike"
+  #   @weight = 15
+  # end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new()
-bike.ring_bell
-bike.accelerate
-bike.accelerate
+bike = Bike.new(:speed => 0, :direction => "north")
+# bike.ring_bell
+# bike.accelerate
+# bike.accelerate
+bike.speed
